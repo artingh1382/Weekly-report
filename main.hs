2f391ddd03f197a5cbc2494b8c3aea7cbf012ebe
artin <artin.gh1382@gmail.com>
2021-10-09 01:07:09 +0330

    the code works but has a lot of duplicate functions in it

--- main.hs
+++ main.hs
@@ -1,5 +1,3 @@
---function to compute tests and times
---
 type Lesson = String
 type Time = Int
 type Tests = Int
@@ -11,81 +9,45 @@ lesson topic time tests = (Topic topic aTime aTests)
   where aTime= sum time
         aTests = sum tests
 
+
 getTopic :: Topic -> Lesson
 getTopic (Topic topic _ _) = topic
+
 
 getTime :: Topic -> Time
 getTime (Topic _ time _) = time
 
+
 getTests :: Topic -> Tests
 getTests (Topic _ _ tests) = tests
 
---allTests :: [Topic] -> Tests
+
+allTests :: [Topic] -> Tests
+allTests topicList = sum $ map getTests topicList
 
 
---allTimes :: [Topic] -> Time
+allTimes :: [Topic] -> Time
+allTimes topicList = sum $ map getTime topicList
+
+
+topicPlusTime :: [Topic] -> [String]
+topicPlusTime topicList = map readable zipped
+  where names = map getTopic topicList
+        times = map getTime topicList
+        zipped = zip names times
+        readable = \pair -> show (fst pair) ++ " : " ++ show (snd pair)
+
+
+topicPlusTest :: [Topic] -> [String]
+topicPlusTest topicList = map readable zipped
+  where names = map getTopic topicList
+        tests = map getTests topicList
+        zipped = zip names tests
+        readable = \pair -> show (fst pair) ++ " : " ++ show (snd pair)
+
+
 percent :: Float -> Float -> Float
-percent c a = (((3*c) - wrong)/ (3*a)) * 100
+percent c a = (((3*c) - wrong) / (3*a)) * 100
   where wrong = a-c
-{-# LANGUAGE MultiParamTypeClasses #-}
 
-type Minute = Int
-type Hour = Int
-
--- a new data type named Time that has has two parts: Hour and minte
-type Time = (Hour,Minute)
-
-
--- the getTime fucntion taks a Time and returns a String in return
-getTime :: Time -> String
-getTime (h,m) = show h ++ "h" ++ show m ++ "m"
-
--- a function which takes numeric value and converts it into Time
-convertToTime :: Int -> Time
-convertToTime 0 = (0,0)
-convertToTime n = (hour,minute)
-  where hour = n `div` 60
-        minute = n - (hour * 60)
-
-
--- the convewrtTimeToMinutes is the inverse of our convertToTime function and gives us the summed minuts as a result
-convertTimeToMinutes :: Time -> Int
-convertTimeToMinutes (h,m) = (h * 60) + m
-
--- new data type representing days
-
-type Months = Int
-type Days = Int
-
-type Date = (Months,Days)
-
-getMonth :: Date-> String
-getMonth (m,d) = show m ++ " Months" ++ " " ++ show d ++ " Days"
-
-convertToDays :: Int -> Date
-convertToDays 0 = (0,0)
-convertToDays n = (months,days)
-  where months = n `div` 30
-        days = n - (months * 30)
-
-convertDateToDays :: Date -> Int
-convertDateToDays (m,d) = (m * 30) + d
-
-class (Num a, Integral a) => ModArith a where
-  convertIntToType :: a -> (a,a)
-  convertIntToType 0 = (0,0)
-  convertIntToType n = (months,days)
-    where months = n `div` 30
-          days = n - (months * 30)
-
-  convertTypeToInt :: a -> a
-  --convertTypeToInt (h,m) =
-  showType :: a -> String
-
---instance ModArith Date where
---  convertIntToType 0 = Date 0 0
---  convertIntToType n = Date months days
---    where months = n `div` 30
---          days = n - (months * 30)
-
-
+--add comments and refactor duplicate code with new functions that take functions as arguments
