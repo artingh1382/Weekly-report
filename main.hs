4b9abecf512e29e8bfcd610d8f5e390d5b435f77
artin <artin.gh1382@gmail.com>
2021-10-10 01:16:13 +0330

    refactored two functions and added comments

--- main.hs
+++ main.hs
@@ -1,33 +1,39 @@
+import           Data.List
+import qualified Data.Map       as M
+import           Data.Monoid
+import           Data.Semigroup
+
+-- using type synonyms to make type signatures help understand the transitions accuring in the function
 type Lesson = String
 type Time = Int
 type Tests = Int
 
 data Topic = Topic Lesson Time Tests
 
+-- a function that takes a String and two Lists of Integer then returns a Topic
 lesson :: Lesson -> [Time] -> [Tests] -> Topic
 lesson topic time tests = (Topic topic aTime aTests)
   where aTime= sum time
         aTests = sum tests
 
 
+-- gets the Lesson (String)
 getTopic :: Topic -> Lesson
 getTopic (Topic topic _ _) = topic
 
 
+-- gets the Time (Int)
 getTime :: Topic -> Time
 getTime (Topic _ time _) = time
 
-
+-- gets the Tests(Int)
 getTests :: Topic -> Tests
 getTests (Topic _ _ tests) = tests
 
 
-allTests :: [Topic] -> Tests
-allTests topicList = sum $ map getTests topicList
-
-
-allTimes :: [Topic] -> Time
-allTimes topicList = sum $ map getTime topicList
+-- takes a list and a function on the list then maps that function and sums the values
+allQuant :: Num a => [Topic] -> (Topic -> a) -> a
+allQuant topicList f = sum $ map f topicList
 
 
 -- takes either one of the getTime or getTests function and then returns a nice visual list
