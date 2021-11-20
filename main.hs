ce48c5ac34a85623b39db5df5ead1c23b7a899ea
artin <artin.gh1382@gmail.com>
2021-11-20 11:45:28 +0330

    the Lex Data type has issues

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import           Data.List
+import           Data.List.Split
 import qualified Data.Map           as M
 import           Data.Monoid
 import           Data.Semigroup
@@ -9,11 +10,49 @@ type Lesson = String
 type Time = Int
 type Tests = Int
 
-data Books = Dis | Phy | Cal | Geo | Che | Theo | Ara | Lit deriving (Show, Eq)
+data Books = Dis | Phy | Cal | Geo | Che | The | Ara | Lit deriving Eq
 
-data Days = Sat | Sun | Mon | Tue | Wed | Thu | Fri deriving (Show, Eq)
+data Days = Sat | Sun | Mon | Tue | Wed | Thu | Fri deriving Eq
 
-data Topic = Topic Lesson Time Tests
+data Topic = Topic Lesson Time Tests | Empty
+
+data Lex = Sep | Days | Books deriving (Eq)
+
+
+class Parts a where
+  parse :: a -> Lex
+  convert :: a -> String
+
+instance Show Books where
+  show Dis = "Discrete"
+  show Phy = "Physics"
+  show Cal = "Calculus"
+  show Geo = "Geometry"
+  show Che = "Chemistry"
+  show The = "Theology"
+  show Ara = "Arabic"
+  show Lit = "Literature"
+
+
+instance Show Days where
+  show Sat = "Saturday"
+  show Sun = "Sunday"
+  show Mon = "Monday"
+  show Tue = "Tuesday"
+  show Wed = "Wednesday"
+  show Thu = "Thursday"
+  show Fri = "Friday"
+
+
+instance Show Lex where
+  show Days  = show Days
+  show Books = show Books
+  show Sep   = "&"
+
+
+
+showLex' :: Lex -> String
+showLex' lex = take 3 $ show lex
 
 
 lesson :: Lesson -> [(Time,Tests)] -> Topic
@@ -48,7 +87,7 @@ topicAndQuant topicList f = map together zipped
   where names = map getTopic topicList
         quant = map f topicList
         zipped = zip names quant
-        together = \pair -> fst pair ++ " : " ++ show (snd pair)
+        together pair = fst pair ++ " : " ++ show (snd pair)
 
 --topicAndQuant :: [Topic] -> (Topic -> Int) -> String
 --topicAndQuant topicList f = mconcat $ map together zipped
@@ -66,26 +105,67 @@ percent e w a = ((c - (w/3)) / a) * 100
 --
 toFloatIO :: String -> IO Float
 toFloatIO x = return float
-  where float = (read x) :: Float
+  where float = read x :: Float
 
 toFloat :: String -> Float
 toFloat x = read x :: Float
 
--- TODO fix the IO Computation bug
+topicsInDays :: String -> [[String]]
+topicsInDays day = map (splitOn "&") $ lines day
+
+stListToSt :: Foldable t => t [a] -> [a]
+stListToSt xs = foldr (++) [] xs
+
+--testList = [["My", "name", "is"], ["Barry", "Allen"], ["And", "I'm", "The"], ["Fastest", "man", "alive"]]
+
+--parseFile :: FilePath -> [Topic]
+--parseFile file = do
+--    handle <- readFile file
+--    let parsed = lines file
+
+parser :: [String] -> [Lex]
+parser = undefined
+
+onlyTopics :: [String] -> [String]
+onlyTopics []   = []
+onlyTopics text = filter (/= " ") text
+
+
+stringToLex :: String -> Days
+stringToLex lex = case lex of
+    "Sat" -> Sat
+    "Sun" -> Sun
+    "Mon" -> Mon
+    "Tue" -> Tue
+    "Wed" -> Wed
+    "Thu" -> Thu
+    "Fri" -> Fri
+
 
 main :: IO ()
 main = do
-    putStrLn "number of blank questions: "
-    blanks <- getLine
-    putStrLn "number of wrong questions: "
-    wrongs <- getLine
-    putStrLn "number of all the questions: "
-    all <- getLine
-    let values = map toFloat [blanks, wrongs, all]
-    let e = values !! 0
-    let w = values !! 1
-    let a = values !! 2
-    let result = percent e w a
-    print result
+    file <- readFile "sample.txt"
+    let parsed = stListToSt $ topicsInDays file
+    mapM_ putStrLn parsed
+    --putStrLn file
+
+
+
+-- TODO fix the IO Computation bug
+
+--main :: IO ()
+--main = do
+--    putStrLn "number of blank questions: "
+--    blanks <- getLine
+--    putStrLn "number of wrong questions: "
+--    wrongs <- getLine
+--    putStrLn "number of all the questions: "
+--    all <- getLine
+--    let values = map toFloat [blanks, wrongs, all]
+--    let e = values !! 0
+--    let w = values !! 1
+--    let a = values !! 2
+--    let result = percent e w a
+--    print result
 
 
